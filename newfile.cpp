2ed4505ebd7a8e4d0c6bf1ab82afe387f02e3c8c
Rupam0403 <rupamdas04032003@gmail.com>
2023-06-16 15:25:36 +0530

    publish

---
+++ newfile.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a,i;
+    cout<<"enter the number for table"<<endl;
+    cin>>a;
+    for(i=1;i<=10;i++)
+    {
+        int mul = a*i;
+        cout<<mul<<endl;
+    }
+
+
+    return 0;
+}
